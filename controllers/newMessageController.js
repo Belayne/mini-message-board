1d7d0d1d821c169147f03a967c7adc28792cc5f6
Belayne <37399395+Belayne@users.noreply.github.com>
2025-01-15 18:40:21 +0100

    feat: add new message page functionality

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -4,8 +4,17 @@ function renderNewMsgPage(req, res) {
     res.render("newMessageForm");
 }
 
+function addNewMessage(req, res, next) {
+    const {username, message: text} = req.body;
+    const newMessage = new Message(text, username);
+    messageList.addMessage(newMessage);
+
+    next();
+}
+
 const newMessageController = {
     get: renderNewMsgPage,
+    post: addNewMessage
 }
 
 export default newMessageController;
--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -4,5 +4,9 @@ import newMessageController from "../controllers/newMessageController.js";
 const newMessageRouter = Router();
 
 newMessageRouter.get("", newMessageController.get);
+newMessageRouter.post("", newMessageController.post);
+newMessageRouter.post("", (req, res) => {
+    res.redirect("/");
+})
 
 export default newMessageRouter;
